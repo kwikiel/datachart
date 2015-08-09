00b93366cb7fd32d095c55d528c782d85583735b
Kacper Wikiel <random8902@gmail.com>
2015-08-09 22:45:18 +0200

    Init

---
+++ scraping.py
@@ -0,0 +1,7 @@
+from lxml import html
+import requests
+
+def loaned_amount(lid):
+    page = requests.get("https://bitlendingclub.com/user/index/id/"+str(lid))
+    tree = html.fromstring(page.text)
+    return float(list(tree.xpath('//span[@class="profile-summary-label"]/following-sibling::text()'))[2].split(" ")[0])
